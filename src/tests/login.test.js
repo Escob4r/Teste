ef92b33cf6071832ab205e50477826f4c8d046af
Pablo <phdsilvaa@gmail.com>
2020-12-14 23:21:22 -0300

    Acerto

--- src/tests/login.test.js
+++ src/tests/login.test.js
@@ -32,7 +32,7 @@ test('snapshot', () => {
 
 test('text button login', () => {
     const text = tree.root.findByProps({testID: 'textButton'}).props
-    expect(text.children).toEqual('Errei')
+    expect(text.children).toEqual('Entrar')
 });
 
 test('button click', () => {
